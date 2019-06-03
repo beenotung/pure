64c2f16c53684fb940c88e9ff6ebd4b5fa03a04f
Beeno Tung <aabbcc1241@yahoo.com.hk>
2019-06-04 01:40:39 +0800

    allow script from same hostname

--- pure.js
+++ pure.js
@@ -1,10 +1,29 @@
-console.log('pure is running');
-let origin = window.origin;
+console.debug('pure is running');
+
+function getHost(url) {
+  let a = document.createElement('a');
+  a.href = url;
+  return a.hostname;
+}
+
+let originHost = getHost(window.origin);
+// console.log('originHost:', originHost);
+// console.log('number of scripts:', document.scripts.length);
 for (let i = document.scripts.length - 1; i >= 0; i--) {
   let script = document.scripts.item(i);
+  if (!script.src) {
+    continue;
+  }
+  let host = getHost(script.src);
   if (
-    !script.src
-    || script.src.startsWith(origin)
+    !(
+      host.includes('ads')
+      || host.includes('analytics')
+    ) && (
+      host.endsWith(originHost)
+      || host.includes('cdn')
+      || host.includes('static')
+    )
   ) {
     continue;
   }
